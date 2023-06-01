aeae4e8017b9d5441b0d2a98c7b07a9a0af03658
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2023-06-01 23:41:54 +0900

    Question, Choice admin.py 작성

--- blog/admin.py
+++ blog/admin.py
@@ -7,18 +7,17 @@ admin.site.register(models.Student)
 admin.site.register(models.Book)
 admin.site.register(models.Course)
 
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 0
 
-# @admin.register(Blog)
-# class BlogAdmin(admin.ModelAdmin):
-
-#     # 원하는 필드
-#     list_display = ['title', 'content']
-
-#     # 링크 설정
-#     # list_display_links = ['']
-
-#     # 검색 필드
-#     search_fields=['title','content']
-
-#     # 검색 필터
-#     # list_filter=['is_staff']
+class QuestionAdmin (admin.ModelAdmin):
+    
+    list_display = ( 'question_text' , 'publish_date' , 'was_published_recently' ) # 필드 출력
+    list_filter = ['publish_date'] # 검색 필터
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date information', {'fields': ['publish_date']}),] # 상세 보기 화면
+    inlines = [ChoiceInline] # inline 추가
+        
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Choice)
