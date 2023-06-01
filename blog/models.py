94e6f68bf56d624f409e9b1f5ea9aaf5c50e9cfb
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2023-06-01 23:41:24 +0900

    외래키 관계의 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 
 
 # Create your models here.
@@ -12,7 +14,6 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateField(auto_now_add= True)
     published_at = models.DateField(null= True)
-
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -27,3 +28,16 @@ class Course(models.Model):
     title = models.CharField(max_length=100)
     students = models.ManyToManyField(Student)
     books = models.ForeignKey(Book, on_delete=models.CASCADE)
+    
+class Question(models.Model):
+    question_text = models.CharField(max_length= 200)
+    publish_date = models.DateTimeField( 'date published' )
+
+    def was_published_recently(self):
+        return (self.publish_date < timezone.now() and
+                self.publish_date >= timezone.now() - datetime.timedelta(days= 1))
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length= 200)
+    votes = models.IntegerField(default= 0)
